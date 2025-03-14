fdebc3595eb0530f2a9edb925d7f6f6891da945a
jahirgosu <jahir.naruto@gmail.com>
2025-03-14 17:23:54 -0600

    La barra de vida ahora tiene animación

--- Assets/Scripts/CircularHealthBar.cs
+++ Assets/Scripts/CircularHealthBar.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,20 +10,15 @@ public class CircularHealthBar : MonoBehaviour
     public float maxHealth = 100f;
     public float currentHealth = 100f;
     public float enemyDamage = 25f;
-
-
-
-
+    public float fillSpeedAmount = 1f;
 
     void Start()
     {
         currentHealth = maxHealth;
         circleImage.fillAmount = currentHealth / maxHealth;
-
+        //Color newColor = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+        //DOTween.SetTweensCapacity(2000, 100);
     }
-
-
-
 
     void Update()
     {
@@ -33,14 +29,10 @@ public class CircularHealthBar : MonoBehaviour
             if (currentHealth < 0) currentHealth = 0;
         }
 
-
         // Actualizas la UI
-        circleImage.fillAmount = currentHealth / maxHealth;
-
-
-
+        float targetFillAmount = currentHealth / maxHealth;
+        circleImage.DOFillAmount(targetFillAmount, fillSpeedAmount);
     }
-
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -48,12 +40,21 @@ public class CircularHealthBar : MonoBehaviour
         {
             currentHealth -= enemyDamage;
             if (currentHealth < 0) currentHealth = 0;
-            circleImage.fillAmount = currentHealth / maxHealth;
+            //circleImage.fillAmount = currentHealth / maxHealth;
             if (currentHealth <= 0) Destroy(gameObject);
 
-            circleImage.fillAmount = currentHealth / maxHealth;
+            //circleImage.fillAmount = currentHealth / maxHealth;
+            // DOVirtual.DelayedCall(_damageDelay);
+            //circleImage.DOColor(Color.red, 1f);
+            //StartCoroutine(ColorRedDelay());
 
         }
     }
+
+    //private IEnumerator ColorRedDelay()
+    //{
+      //  yield return new WaitForSeconds(2f);
+      //  circleImage.DOColor(new Color(39f, 248f, 238f, 255f), 1f);
+    //}
 
 }
