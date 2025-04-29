187a79262c245028c62d6fbecb44ea4e4ac33870
jahirgosu <jahir.naruto@gmail.com>
2025-04-28 20:31:00 -0600

    mas cosas

--- Assets/Scripts/LogoAnimation.cs
+++ Assets/Scripts/LogoAnimation.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogoAnimation : MonoBehaviour
 {
+    public Image Logo;
     private void Start()
     {
         StartLogo();
@@ -12,5 +14,6 @@ public class LogoAnimation : MonoBehaviour
     public void StartLogo()
     {
         transform.LeanMoveLocal(new Vector2(0, 74), 1).setEaseOutQuad();
+        Logo.transform.LeanMoveLocal(new Vector2(392, -208), 1).setEaseOutQuad();
     }
 }
