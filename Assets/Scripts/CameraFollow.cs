d01fc9779a01dec4bd9bacb01d56785c0e119a71
jahirgosu <jahir.naruto@gmail.com>
2025-04-28 19:23:02 -0600

    nivel de prueba

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,12 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
+    public float xOffset = 1f;
     public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
